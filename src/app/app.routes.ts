41bf6700e3de5403f94daa56ad821355bb80f759
DEPI-Final-Project <nermein4686@gmail.com>
2024-09-25 21:20:29 +0300

    baic app structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+
+import { NotFoundComponent } from './not-found/not-found.component';
+import { HomeComponent } from './myComponent/home/home.component';
+import { ProductsComponent } from './myComponent/products/products.component';
+import { NavComponent } from './myComponent/nav/nav.component';
+import { ContactUsComponent } from './myComponent/contact-us/contact-us.component';
+import { CartComponent } from './myComponent/cart/cart.component';
+
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'nav', component: NavComponent },
+  { path: 'products', component: ProductsComponent },
+  { path: 'contact', component: ContactUsComponent },
+  { path: 'cart', component: CartComponent },
+
+  { path: '**', component: NotFoundComponent },
+];
