2fdd1db8fa813298846936b42e9152e3c5f74daa
Sarah Salem <sarah.salem18@gamil.com>
2024-09-27 22:07:25 +0300

    adding about component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,7 @@ import { ProductsComponent } from './myComponent/products/products.component';
 import { NavComponent } from './myComponent/nav/nav.component';
 import { ContactUsComponent } from './myComponent/contact-us/contact-us.component';
 import { CartComponent } from './myComponent/cart/cart.component';
+import { AboutComponent } from './myComponent/about/about.component';
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
@@ -14,6 +15,7 @@ export const routes: Routes = [
   { path: 'products', component: ProductsComponent },
   { path: 'contact', component: ContactUsComponent },
   { path: 'cart', component: CartComponent },
+  { path: 'About', component:AboutComponent},
 
   { path: '**', component: NotFoundComponent },
 ];
