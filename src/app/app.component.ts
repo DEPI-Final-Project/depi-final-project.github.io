277aaafada5321c5b77d4a338e70385f1b02753f
rehab <rehabelazab47@gmail.com>
2024-09-30 20:22:19 +0300

    navbar &footer design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavComponent } from "./myComponent/nav/nav.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, NavComponent],
+  // templateUrl: './app.component.html',
+  templateUrl: './myComponent/nav/nav.component.html',
+  // styleUrl: './app.component.css'
+  styleUrl: './myComponent/nav/nav.component.css',
 })
 export class AppComponent {
   title = 'e-commerce';
