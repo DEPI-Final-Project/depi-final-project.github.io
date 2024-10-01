41fdd0c1ebdbcb4f1a4e1c62590aaf38ed09c0be
rehab <rehabelazab47@gmail.com>
2024-10-01 15:29:02 +0300

    add footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { NavComponent } from './myComponent/nav/nav.component';
-
+import { FooterComponent } from './myComponent/footer/footer.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavComponent, RouterModule],
+  imports: [RouterOutlet, NavComponent,FooterComponent],
   templateUrl: './app.component.html',
   // templateUrl: './myComponent/footer/footer.component.html',
   styleUrl: './app.component.css'
