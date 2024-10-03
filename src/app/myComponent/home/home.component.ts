70e77b89849ccc6073ad62c331ddd7ef6fe92e89
SAEED-O511 <saeedosamaradwan@gmail.com>
2024-10-04 02:51:26 +0300

    join now page finshed now you can creat account and make subscribe evrey thing is store

--- src/app/myComponent/home/home.component.ts
+++ src/app/myComponent/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { AppComponent } from "../../app.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [AppComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
