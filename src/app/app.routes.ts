77a092df2e281482b76843b5be4cf6e2b7f3054f
DEPI-Final-Project <nermein4686@gmail.com>
2024-10-06 00:37:02 +0300

    categories component added with styling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
+import { CategoryComponent } from './myComponent/category/category.component';
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   { path: 'home', loadComponent: () => import('./myComponent/home/home.component').then(m => m.HomeComponent) },
   { path: 'products', loadComponent: () => import('./myComponent/products/products.component').then(m => m.ProductsComponent) },
-  { path: 'cart', loadComponent: () => import('./myComponent/cart/cart.component').then(m => m.CartComponent) },
+  { path: 'category', loadComponent: () => import('./myComponent/category/category.component').then(m => m.CategoryComponent) },
   { path: 'about', loadComponent: () => import('./myComponent/about/about.component').then(m => m.AboutComponent) },
   { path: 'login', loadComponent: () => import('./myComponent/log-in/log-in.component').then(m => m.LogInComponent) },
   { path: 'footer', loadComponent: () => import('./myComponent/footer/footer.component').then(m => m.FooterComponent) },
