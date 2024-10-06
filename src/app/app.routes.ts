b34daa7c105c0633ea2821a3b5e729d12801b3ce
SAEED-O511 <saeedosamaradwan@gmail.com>
2024-10-06 21:26:02 +0300

    now evry thing work well

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,9 +6,12 @@ export const routes: Routes = [
   { path: 'products', loadComponent: () => import('./myComponent/products/products.component').then(m => m.ProductsComponent) },
   { path: 'category', loadComponent: () => import('./myComponent/category/category.component').then(m => m.CategoryComponent) },
   { path: 'about', loadComponent: () => import('./myComponent/about/about.component').then(m => m.AboutComponent) },
+  { path: 'cart', loadComponent: () => import('./myComponent/cart/cart.component').then(m => m.CartComponent) },
+
   { path: 'login', loadComponent: () => import('./myComponent/log-in/log-in.component').then(m => m.LogInComponent) },
   { path: 'footer', loadComponent: () => import('./myComponent/footer/footer.component').then(m => m.FooterComponent) },
   { path: 'payment', loadComponent: () => import('./payment/payment.component').then(m => m.PaymentComponent) },
+
   { path: '**', loadComponent: () => import('./not-found/not-found.component').then(m => m.NotFoundComponent) }
 
 
