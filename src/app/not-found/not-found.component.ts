c51870e012eb689ee3344921df6c5e1d55de0a57
Ahmed Khaled <ahmedeldairy@gmail.com>
2024-10-11 23:15:27 +0300

    fixing angular.json

--- src/app/not-found/not-found.component.ts
+++ src/app/not-found/not-found.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-not-found',
@@ -8,5 +10,10 @@ import { Component } from '@angular/core';
   styleUrl: './not-found.component.css'
 })
 export class NotFoundComponent {
+  constructor(private router: Router) { }
+
+  navigateTo(path: string) {
+    this.router.navigate([path]);
+  }
 
 }
